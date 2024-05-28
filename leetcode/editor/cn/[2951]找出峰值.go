c1066959df84dc9a5338916b221e0cfa85f02abd
fuyunpeng <83266443+fuyunpeng0818@users.noreply.github.com>
2024-05-28 14:19:58 +0800

    2024-05-28

---
+++ leetcode/editor/cn/[2951]找出峰值.go
@@ -0,0 +1,66 @@
+//给你一个下标从 0 开始的数组 mountain 。你的任务是找出数组 mountain 中的所有 峰值。 
+//
+// 以数组形式返回给定数组中 峰值 的下标，顺序不限 。 
+//
+// 注意： 
+//
+// 
+// 峰值 是指一个严格大于其相邻元素的元素。 
+// 数组的第一个和最后一个元素 不 是峰值。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：mountain = [2,4,4]
+//输出：[]
+//解释：mountain[0] 和 mountain[2] 不可能是峰值，因为它们是数组的第一个和最后一个元素。
+//mountain[1] 也不可能是峰值，因为它不严格大于 mountain[2] 。
+//因此，答案为 [] 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：mountain = [1,4,3,8,5]
+//输出：[1,3]
+//解释：mountain[0] 和 mountain[4] 不可能是峰值，因为它们是数组的第一个和最后一个元素。
+//mountain[2] 也不可能是峰值，因为它不严格大于 mountain[3] 和 mountain[1] 。
+//但是 mountain[1] 和 mountain[3] 严格大于它们的相邻元素。
+//因此，答案是 [1,3] 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 3 <= mountain.length <= 100 
+// 1 <= mountain[i] <= 100 
+// 
+//
+// Related Topics 数组 枚举 👍 16 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func findPeaks(mountain []int) []int {
+    if len(mountain) <= 2 {
+        return nil
+    }
+    p1, p2, p3 := 0, 0, 0
+    res := make([]int, 0)
+    i := 0
+    for i<len(mountain)-2 {
+        p1 = mountain[i]
+        p2 = mountain[i+1]
+        p3 = mountain[i+2]
+        if p2 > p1 && p2 > p3 {
+            res = append(res, i+1)
+        }
+        i++
+    }
+    return res
+}
+//leetcode submit region end(Prohibit modification and deletion)
